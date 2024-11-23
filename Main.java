ec6c92521091aeb94eca20b36ff53c7959ac8294
Brightcoz <marcuswilliam021031@gmail.com>
2024-11-23 05:55:47 +0000

    updated

--- Main.java
+++ Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         String num1 = 22;
-        String num2 = 44
+        String num2 = 44;
         System.out.println(num1 + num2);
     }
 }
