96bcb39fad20760384b5171bd26618bfd270aea4
Brightcoz <marcuswilliam021031@gmail.com>
2024-11-23 05:51:26 +0000

    updated

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        String myName = "Makinde William";
+        String myName = "Makinde William ";
         String about = "I am a software engineer, i developing software, singing, gymning and studying";
         System.out.println(myName + about);
     }
