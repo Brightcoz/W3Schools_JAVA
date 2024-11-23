36ecf0d3e32e68c6545b48c410c090456364beee
Brightcoz <marcuswilliam021031@gmail.com>
2024-11-23 15:33:08 +0000

    updated

--- Main.java
+++ Main.java
@@ -1,7 +1,5 @@
 public class Main {
-    public static void main(String[] args) {
-        int age = 22, matricNum = 2233;
-        String name = "My name is Makinde Williams";
-        System.out.println(name + " " + ",I am" + age + " " + "years old");
+    public static void main(string[] args) {
+        byte x = 
     }
 }
